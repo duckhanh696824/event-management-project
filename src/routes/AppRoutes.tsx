2a89790f2ec31ca3b6f7ba5dab73a5d7d214a10f
duc_khanh <nguyenduckhanh12tn1nh2021@gmail.com>
2025-01-23 15:09:14 +0700

    Add event detail page for user

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,5 +1,6 @@
 import LoginPage from "pages/login/Login";
 import Contact from "pages/user/Contact";
+import EventDetailPage from "pages/user/EventDetail";
 import Home from "pages/user/Home";
 import React from "react";
 import { Routes, Route } from "react-router-dom";
@@ -11,6 +12,7 @@ const AppRoutes = () => {
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<LoginPage/>} />
       <Route path="/contact" element={<Contact/>} />
+      <Route path="/event/:id" element={<EventDetailPage/>} />
     </Routes>
   );
 };
