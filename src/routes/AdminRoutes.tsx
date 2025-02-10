4c779fb403aff77e528b3d719feb06efaa61a78e
duc_khanh <nguyenduckhanh12tn1nh2021@gmail.com>
2025-02-10 22:38:21 +0700

    chore: Change the path to the Event Update page

--- src/routes/AdminRoutes.tsx
+++ src/routes/AdminRoutes.tsx
@@ -1,7 +1,13 @@
 import EventManagement from "components/event/admin/EventListAdmin";
-import CreateEventPage from "pages/admin/CreateEventPage";
+
 import DetailEventAdminPage from "pages/admin/DetailEventAdminPage";
-import UpdateEventPage from "pages/admin/UpdateEventPage";
+import UpdateEventPage from "pages/admin/event/UpdateEventPage";
+
+import CreateEventPage from "pages/admin/event/CreateEventPage";
+import DetailEventAdminPage from "pages/admin/DetailEventAdminPage";
+import EventTypeListPage from "pages/admin/event-type/EventTypeListPage";
+import EventRegistrationPage from "pages/admin/EventRegistrationPage";
+
 import AdminLayout from "pages/layouts/AdminLayout";
 import React from "react";
 import { Routes, Route, useParams } from 'react-router-dom';
